da11d54dd3659a2180d3515d640af1478ddf66cc
yoshimasa1101 <yo50si11@gmail.com>
2025-10-18 16:35:04 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,3 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>TASQ JAPAN</title>
-        <meta name="description" content="逆オークションMVP" />
-      </Head>
-      <main style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
-        <h1>TASQ JAPAN</h1>
-        <p>逆オークションMVPへようこそ</p>
-      </main>
-    </>
-  );
-}
+git add pages/index.tsx
+git commit -m "fix: remove unused imports for minimal build"
+git push origin main
